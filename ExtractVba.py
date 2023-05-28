667b4646dced3701719eb15e965b2839c28bc130
pg <pg@pg.com>
2023-05-28 16:03:29 +0530

    Partially added feature to remove Macros and save new file

--- ExtractVba.py
+++ ExtractVba.py
@@ -1,26 +1,174 @@
-from bitstring import BitArray
 
+#
+#
+#
+#
+#
+#
+#
+#
 
+#CompressedContainer=b'\x01\x16\x01\x00'					#Array of bytes holding compressed data
+#DecompressedBuffer=b''#
 
+#if CompressedContainer[0]!=1:
+#	print('ERROR')
+#	#print(CompressedContainer[0])
+#Chunks=[]
+#for i in range(1,len(CompressedContainer)):
+#	header=CompressedContainer[i:i+2]#
 
+#	CompressedChunkSize=ExtractCompressedChunkSize#
 
+#	Chunks.append(CompressedContainer[i:i+CompressedChunkSize])
+#	i=i+CompressedChunkSize#?????????????????????????????#
 
+#for Chunk in Chunks:
+#	DecompressChunk(Chunk)#
 
+#def ExtractCompressedChunkSize(header):
+#	bits = ''.join(format(byte, '08b') for byte in header)
+#	return(int(bits[:12],2)+3)				#First 12 bits represent the number of bytes minus 3#
 
+#def ExtractCompressedChunkFlag(header):
+#	bits = ''.join(format(byte, '08b') for byte in header)
+#	return(int(bits[-1]))#
 
+#def DecompressChunk(Chunk):
+#	header=Chunk[0:2]
+#	CompressedSize=ExtractCompressedChunkSize(header)
+#	CompressedFlag=ExtractCompressedChunkFlag(header)
+#	if CompressedFlag ==1:
+#		DecompressingArrayOfTokenSequence(chunk)
+#	else:
+#		DecompressingARawChunk(Chunk)#
 
+#def DecompressingARawChunk(Chunk):
+#	CompressedChunkData=Chunk[2:4098] #?????????????????USe len(Chunk)??
+#	DecompressedBuffer=DecompressedBuffer+CompressedChunkData#
 
-CompressedContainer=b'\x01\x16\x01\x00'					#Array of bytes holding compressed data
-if CompressedContainer[0]!=1:
-	print('ERROR')
-	#print(CompressedContainer[0])
-Chunks=[]
-for i in range(1,len(CompressedContainer)):
-	header=CompressedContainer[i:i+2]
-	print(header)
-	#CompressedChunkSize=BitArray(hex=header)
-	bits = ''.join(format(byte, '08b') for byte in header)
-	print(int(bits[:12],2))
-	#CompressedChunkSize=2**(int(bits[:12]))
-	#print(CompressedChunkSize)
-	break
+#def DecompresssingArrayOfTokenSequence(Chunk):
+#	CompressedChunkData=[] # Array of Token Sequences
+#	for j in range(2,len(Chunk)):
+#		FlagByte=format(Chunk[j],'08b')
+#		FlagByte=FlagByte[::-1]											#MSB denotes last token
+#		nbytes=FlagByte.count('1')*2 + FlagByte.count('0')*1			#FlabByte 1 means CopyToken(2 Bytes) and 0 means LiteralToken(1 Byte)
+#		CompressedChunkData.append(Chunk[j:j+nbytes+1])					# +1 to compensate for the header#
+#
+
+#	for TokenSequence in CompressedChunkData:
+#		DecompressingATokenSequence(TokenSequence)#
+
+#def DecompressingATokenSequence(TokenSequence):
+#	Tokens=[]
+#	FlagByte=format(TokenSequence[0],'08b')
+#	FlagByte=[::-1]														# Reversing FlagByte
+#	fb=0
+#	for i in range(1,len(TokenSequence)):
+#		if FlagByte[fb]==0:
+#			Tokens.append(TokenSequence[i])		
+#			i=i+1
+#		else:
+#			Tokens.append(TokenSequence[i:i+2])
+#			i=i+2
+#	for index in range(len(Tokens)):
+#		DecompressingAToken(Tokens[index],index,FlagByte)#
+
+#def DecompressingAToken(Token,index,FlabByte):
+#	if FlagByte[index]==0:												#Literal token
+#		DecompressedBuffer=DecompressedBuffer+Token
+#	else:																#Copy Token
+#		Offset, Length = UnpackCopyToken(Token)
+#		CopySource= len(DecompressedBuffer)-Offset
+#		ByteCopy(CopySource,Length)#
+#
+
+#def UnpackCopyToken(Token):
+#	LengthMask, OffsetMask, BitCount=CopyTokenHelp(Token)
+#	Length= Token & LengthMask
+#	temp1 = Token & OffsetMask
+#	temp2 = 16 - BitCount
+#	Offset = (temp1 >> temp2) +1
+#	return(Offset,Length)#
+#
+
+#def CopyTokenHelp(Token): #
+#
+
+#def ByteCopy(CopySource,ByteCount):
+#	bytecpy=DecompressedBuffer[CopySource:CopySource+ByteCount]
+#	DecompressedBuffer=DecompressedBuffer+bytecpy#
+#
+
+#	
+#	
+import math
+def P23Ord(value):
+    if type(value) == int:
+        return value
+    else:
+        return ord(value)
+def ParseTokenSequence(data):
+    flags = P23Ord(data[0])
+    data = data[1:]
+    result = []
+    for mask in [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80]:
+        if len(data) > 0:
+            if flags & mask:
+                result.append(data[0:2])
+                data = data[2:]
+            else:
+                result.append(data[0])
+                data = data[1:]
+    return result, data
+def OffsetBits(data):
+    numberOfBits = int(math.ceil(math.log(len(data), 2)))
+    if numberOfBits < 4:
+        numberOfBits = 4
+    elif numberOfBits > 12:
+        numberOfBits = 12
+    return numberOfBits
+
+def DecompressChunk(compressedChunk):
+    if len(compressedChunk) < 2:
+        return None, None
+    header = P23Ord(compressedChunk[0]) + P23Ord(compressedChunk[1]) * 0x100
+    size = (header & 0x0FFF) + 3
+    flagCompressed = header & 0x8000
+    data = compressedChunk[2:2 + size - 2]
+
+    if flagCompressed == 0:
+        return data.decode(errors='ignore'), compressedChunk[size:]
+
+    decompressedChunk = ''
+    while len(data) != 0:
+        tokens, data = ParseTokenSequence(data)
+        for token in tokens:
+            if type(token) == int:
+                decompressedChunk += chr(token)
+            elif len(token) == 1:
+                decompressedChunk += token
+            else:
+                if decompressedChunk == '':
+                    return None, None
+                numberOfOffsetBits = OffsetBits(decompressedChunk)
+                copyToken = P23Ord(token[0]) + P23Ord(token[1]) * 0x100
+                offset = 1 + (copyToken >> (16 - numberOfOffsetBits))
+                length = 3 + (((copyToken << numberOfOffsetBits) & 0xFFFF) >> numberOfOffsetBits)
+                copy = decompressedChunk[-offset:]
+                copy = copy[0:length]
+                lengthCopy = len(copy)
+                while length > lengthCopy: #a#
+                    if length - lengthCopy >= lengthCopy:
+                        copy += copy[0:lengthCopy]
+                        length -= lengthCopy
+                    else:
+                        copy += copy[0:length - lengthCopy]
+                        length -= length - lengthCopy
+                decompressedChunk += copy
+    return decompressedChunk, compressedChunk[size:]
+
+with open('C:\\Users\\husky\\Downloads\\dumped\\Macros\\VBA\\Module1.vba','rb') as f:
+    Compresseddata=f.read()
+DecompressedChunk=DecompressChunk(Compresseddata[3138:])
+print(DecompressedChunk[0])
